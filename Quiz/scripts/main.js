7367595fc3c6077186d65551058b351022544e32
Huaira Nimrod <nimax29@gmail.com>
2023-02-13 18:28:23 -0700

    quiz

--- Quiz/scripts/main.js
+++ Quiz/scripts/main.js
@@ -1,5 +1,7 @@
 const url = "https://huairanimrod.github.io/TA/questions.json";
 
+var answer = [];
+let userAns = [];
 async function getQuestionsData(url) {
     const response = await fetch(url);
     const data = await response.json();
@@ -9,7 +11,7 @@ async function getQuestionsData(url) {
 
   getQuestionsData(url);
 
-
+  
   const displayPresentations = (questions) =>{
 
     const exam = document.querySelector('div.quiz');
@@ -18,20 +20,27 @@ async function getQuestionsData(url) {
         let question= document.createElement('div');
         let h4 = document.createElement('h4');
         
+        
+  
 
         h4.textContent = premise.question;
+        answer.push(premise.answer);
         question.appendChild(h4);
+        var i =0;
         // option.textContent = premise.options;
         (premise.options).forEach(element => {
-
+            
             let option= document.createElement('div');
             let input = document.createElement('input');
             let label = document.createElement('label');
             
             input.type = "radio";
             input.id = element;
-            input.value = element;
+            input.value = i;
             input.name = premise.answer;
+            input.addEventListener("click", function(){
+            userAns.push(input.value);
+            });
 
             label.for = element;
             label.innerText = element;
@@ -39,14 +48,45 @@ async function getQuestionsData(url) {
             option.appendChild(input);
             option.appendChild(label);
             question.appendChild(option);
+           
+            i++;
+
+            
        });
 
        
         // question.appendChild(option);
         exam.appendChild(question);
 
-
+       
     });
+       
 
+};
 
-  };
+function grade(){
+
+    console.log(answer);
+    console.log(userAns);
+
+    
+
+    if(answer.length!= userAns.length){
+        if(confirm("You have remaining questions without answer, are you sure you want to submit it?")){
+            nextWindow();
+        };
+
+    }
+    
+}
+
+function nextWindow(){
+
+    let score = "(:";
+    document.querySelector('div.quiz').style.display = "none";
+    document.querySelector('button').style.display = "none";
+    const span = document.querySelector('span');
+    span.innerText = `Your score is score ${score}`;
+
+}
+
